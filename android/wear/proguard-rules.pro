505d22ab8b94029673e294b4034ae99ffe616e27
daitr2024 <daitr2024@gmail.com>
2026-04-06 00:24:10 +0300

    Clean up: Keep only legal/privacy docs. Source code moved to private repo.

--- android/wear/proguard-rules.pro
+++
@@ -1,4 +0,0 @@
-# ProGuard rules for Wear OS module
--keep class com.daitr2024.personalityai.wear.** { *; }
--keep class com.google.android.gms.wearable.** { *; }
--dontwarn com.google.android.gms.**
--- lib/features/settings/presentation/providers/backup_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../../core/services/backup_service.dart';
-
-/// Provider for backup service
-final backupServiceProvider = Provider<BackupService>((ref) {
-  return BackupService();
-});
